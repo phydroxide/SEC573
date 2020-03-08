9477f107f32fdc976c3a5c32fb0a629facd37157
philip_waters <philip.waters@oracle.com>
2020-03-08 14:00:11 -0600

    Using a buffered generator

--- PS4/FindFile.py
+++ PS4/FindFile.py
@@ -1,27 +1,59 @@
 import re
 import os
-s_bytes=2 #int(input("Match (number) of beginning bytes:"))
-e_bytes=2 #int(input("Match (number) of ending bytes:"))
+import time
 
-with open("input/photo.jpg", "rb") as input_handler:
+global buffer_string
+buffer_string=b""
+buffer_size=1024
+
+def bufferedSample(filehandle, size=512):
+    while True:
+        nextpart=filehandle.read(size)
+        if not nextpart:
+            break
+        yield nextpart
+
+     
+with open("input/seq.hex", "rb") as input_handler:
+    print("ready")
+    for sample in bufferedSample(input_handler, 10):
+        print(sample)
+        print(dir(sample))
+        print(type(sample))
+        b"".join([buffer_string,sample])
+        if (len(buffer_string) > buffer_size):
+            chop_before=len(buffer_string)-buffer_size
+            buffer_string=buffer_string[chop_before:buffer_size:]
+        print("Length now {}".format(len(buffer_string)))
+
+        
+s_bytes=int(input("Match (number) of beginning bytes:"))
+e_bytes=int(input("Match (number) of ending bytes:"))
+
+with open("input/ABCD.txt", "rb") as input_handler:
     pattern=input_handler.read()
 
 pat_len=int(len(pattern))
+
 opening=pattern[:s_bytes:]
 type(pat_len)
 type(e_bytes)
 begin=int(pat_len-e_bytes)
 closing=pattern[begin::]
 print("From {} to {}".format(opening,closing))
+expression=opening+b".*"+closing
+print( expression)        
         
-with open("input/image2.raw", "rb") as input_handler:
-    image=input_handler.read() 
-    
-expression=opening+b"..*"+closing
-print(expression)
-contents=re.findall(expression, image, re.DOTALL )   
-print(contents)
-with open("output/photo.jpg", "wb") as output_handler:
+contents=[]
+with open("input/seq.hex", "rb") as image_handler:
+    for sample in bufferedSample(image_handler, 10):
+        print("{}:{}".format(expression,sample))
+        contents.extend(re.findall(expression, sample, re.DOTALL ))   
+        print(contents)
+        time.sleep(1)
+        print("{} entries found".format(len(contents)))
+
+with open("output/message.txt", "wb") as output_handler:
     for bytesfound in contents:
         output_handler.write(bytesfound)
        
