4e462b904f30244edb7844c2103af28b3142f9d0
philip_waters <philip.waters@oracle.com>
2020-03-09 02:18:25 -0600

    Works to extract a minecraft file, right up until it crashes into a non-raw input for headers

--- PS4/FindFile.py
+++ PS4/FindFile.py
@@ -1,7 +1,6 @@
 import re
 import time
 import hashlib
-from collections import defaultdict
 
 #Yield Generator to iterate over file reading small chunks
 def bufferedSample(filehandle, size=512):
@@ -31,45 +30,52 @@ while True:
 #else:
 #   buffer_limit=file_size
 
-buffer_limit=2*file_size
 
 #Read in pattern to match and capture user's desires on where to start/end
-with open("input/LMNOPQR_S.txt", "rb") as input_handler:
+pattern_file="input/BedrockUserSettingsStorage"
+with open(pattern_file, "rb") as input_handler:
     pattern=input_handler.read()
 
 pattern_length=len(pattern)
 
 while True:
-    s_bytes=int(input("Match (number) of beginning bytes < {}:".format(pattern_length)))
-    if s_bytes > pattern_length-1:
-        print("Select beginning bytes leaving at least one to anchor at the end. Try Again.")
-    elif s_bytes <= 0:
-        print("Must Select at least one start byte. Try again.")
-    else:
-        break
+    try:
+        s_bytes=int(input("Match (number) of beginning bytes < {}:".format(pattern_length)))
+        if s_bytes > pattern_length-1:
+            print("Select beginning bytes leaving at least one to anchor at the end. Try Again.")
+        elif s_bytes <= 0:
+            print("Must Select at least one start byte. Try again.")
+        else:
+            break
+    except:
+        print("Invalid Input")
     
 
 remain=pattern_length-s_bytes
 while True:
-    e_bytes=int(input("Match (number) of ending bytes <= {}:".format(remain)))
-    if e_bytes > remain:
-        print("Not Enough Bytes Left. Try Again.")
-    elif e_bytes <=0:
-        print("Must select some number of ending bytes. Try again.")
-    else:
-        break
-
+    try:
+        e_bytes=int(input("Match (number) of ending bytes <= {}:".format(remain)))
+        if e_bytes > remain:
+            print("Not Enough Bytes Left. Try Again.")
+        elif e_bytes <=0:
+            print("Must select some number of ending bytes. Try again.")
+        else:
+            break
+    except:
+        print("Invalid Input")
+        
+    
 start_pattern=pattern[:s_bytes:]
 end_index=int(pattern_length-e_bytes)
 end_pattern=pattern[end_index::]
 
-print("Searching for patterns starting {} and ending {}".format(start_pattern,end_pattern))
+print("Searching for patterns starting \n{} and ending \n{}\n\n".format(start_pattern,end_pattern))
 time.sleep(1)
 
 
 #Construct regular expression to search
-expression=start_pattern+b".*"+end_pattern
-print( expression)        
+re_expression=br''.join([start_pattern,br"..*",end_pattern]) #b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" #+end_pattern
+print(re_expression)
 
 #emptyhash=hashlib.md5()
 #emptyhash.update(b"")
@@ -77,32 +83,61 @@ print( expression)
             
 contents={}
 #contents[empty_digest]=b""
-last_chunk=b''
- 
-with open("input/seq.hex", "rb") as image_handler:
+last_chunk=bytearray(br'')
+print("Byte Builder zeroed out to length {}".format(len(last_chunk)))
+buffer_limit=2*file_size
+
+#imagefile="/media/pwaters/56e05090-43ef-4899-826f-ccf168689305/PS4/sdb27copy"
+imagefile="input/MineCraft"
+with open(imagefile, "rb") as image_handler:
     for image_part in bufferedSample(image_handler, file_size):
-        buffer_string=b"".join([last_chunk,image_part])
+        buffer_string=br''.join([last_chunk,image_part])
         new_length=len(buffer_string)
-        trunc_start=new_length-buffer_limit
+        print("Total Byte Builder Length {}".format(new_length))
+        #Show Start and End of current sample read
+        #5243136
+        #grid_start=re.findall(b"..",buffer_string[0:256:])
+        #grid_stop=re.findall(b"..",buffer_string[-260::])
+        
+
+        start_list=[x.hex() for x in re.findall(b".",buffer_string[0:256:], re.DOTALL)]
+        for row in range(0,16):
+            print(start_list[(row*16):(row+1)*16])
+            
+        print("........")
+        
+        #One off troubleshooting adjustment for hex display alignment with linux dd
+        #e.g (b".",buffer_string[-258::]
+        #The file handle reader is eating my 0x0a newlines and the grid goes out of alignment.
+        #Solve with re.DOTALL
+        stop_list=[x.hex() for x in re.findall(b".",buffer_string[-256::], re.DOTALL)]
+        for row in range(0,16):
+            print(stop_list[(row*16):(row+1)*16])
+            
+        print("Sample Chunks of lengths {} and {}".format(len(start_list),len(stop_list)))
+
         last_chunk=buffer_string
         if (new_length > buffer_limit):
+            trunc_start=new_length-buffer_limit 
+            print("Truncate Image part of length {} starting index slice {}:".format(new_length,trunc_start))
             buffer_string=bytes(buffer_string[trunc_start::])
-       
-        results=re.findall(expression, buffer_string, re.DOTALL)
+            
+        #print(buffer_string)
+        results=re.findall(br"".join([re_expression]), buffer_string, re.DOTALL)
         
+        print("Done")
+        time.sleep(1)
         #Take results from this chunk and append to found dictionary
         for result in results:
-            print(result)
             hasher=hashlib.md5()
             hasher.update(result)
             result_hash=hasher.hexdigest()
             contents[result_hash]=bytes(result)         
     
-print(contents)
+#print(contents)
 for entry in contents.values():
-    print("{}:{}:{}".format(entry, type(entry), id(entry)))
+    print("Length {} | Type {} | ID {}".format(len(entry), type(entry), id(entry)))
         
-print(contents.values())
 for index,bytesfound in enumerate(contents.values()):
     with open("output/message" + str(index) +".txt", "wb") as output_handler:
         output_handler.write(bytesfound)
