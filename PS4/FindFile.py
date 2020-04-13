92d43b40d60d9f50367d925d477f3bb268ce247c
philip_waters <philip.waters@oracle.com>
2020-04-13 16:50:28 -0600

    Broken. It is not effective to use Regular Expressions to extract a pattern of a certain size. But there were enough innovations in this iteration I want to keep it

--- PS4/FindFile.py
+++ PS4/FindFile.py
@@ -1,184 +1,203 @@
 import re
 import time
 import hashlib
-from _ast import Try
+import gc
+from copy import deepcopy
+
+from copy import copy
+import os
+import struct
+
+#Open Sample File
+#Open Device 
+#Find instance where file begins
+#Find File Size
+#Start from beginning and grab file of size x
+#Write file with Hash name
+#Look above and below for pfskey
+
+
 
 #Yield Generator to iterate over file reading small chunks
-def bufferedSample(filehandle, size=512):
+def bufferedSample(filehandle, size=32768):
     while True:
         nextpart=filehandle.read(size)
         if not nextpart:
             break
         yield nextpart
-
-while True: 
-    try:
-        file_size=int(input("How big is the file you're looking for?"))
-        break
-    except:
-        print("Expecting Integer Number")
-#TODO Start Byte offset
-#length 16129000000 slice 15875000000:
-#and cleanup in yield
-
-#while True: 
-#    try:
-#        read_slice=int(input("Read file in increments of how many bytes?"))
-#        break
-#    except:
-#        print("Expecting Integer Number")
         
-
-#if file_size<read_slice:
-#   buffer_limit=read_slice+file_size
-#else:
-#   buffer_limit=file_size
-
-
-#Read in pattern to match and capture user's desires on where to start/end
-#pattern_file="input/BedrockUserSettingsStorage"
-#pattern_file="input/BedrockUserSettingsStorage.bin"
-pattern_file="input/MineCraft"
-
-with open(pattern_file, "rb") as input_handler:
-    pattern=input_handler.read()
-
-pattern_length=len(pattern)
-
-while True:
-    try:
-        s_bytes=int(input("Match (number) of beginning bytes < {}:".format(pattern_length)))
-        if s_bytes > pattern_length-1:
-            print("Select beginning bytes leaving at least one to anchor at the end. Try Again.")
-        elif s_bytes <= 0:
-            print("Must Select at least one start byte. Try again.")
-        else:
-            break
-    except:
-        print("Invalid Input")
+def deviceNibbler(nibblefile, start=0, length=3):
+    with open(nibblefile, "rb") as nibble_handler:
+        nibble_handler.seek(start)
+        nibble=nibble_handler.read(length)
+    gc.collect()
+    return nibble
+   
+def backNibbles(nibblefile, windowsize=10, skipbyte=0):
+    start_counter=skipbyte
+    buffer_length=windowsize*2
     
-
-remain=pattern_length-s_bytes
-while True:
-    try:
-        e_bytes=int(input("Match (number) of ending bytes <= {}:".format(remain)))
-        if e_bytes > remain:
-            print("Not Enough Bytes Left. Try Again.")
-        elif e_bytes <=0:
-            print("Must select some number of ending bytes. Try again.")
-        else:
-            break
-    except:
-        print("Invalid Input")
+    with open(nibblefile, "rb") as size_checker:
+        file_length=os.fstat(size_checker.fileno()).st_size    
+    
+    while start_counter < file_length:
+        nibble_buffer=deviceNibbler(nibblefile, start_counter, buffer_length)
+        start_counter=start_counter+windowsize
+        gc.collect()
+        yield(nibble_buffer)
         
-    
-start_pattern=pattern[:s_bytes:]
-end_index=int(pattern_length-e_bytes)
-end_pattern=pattern[end_index::]
-
-print("Searching for patterns starting \n{} and ending \n{}\n\n".format(start_pattern,end_pattern))
-time.sleep(1)
-
-#override for my minecraft data
-end_pattern=b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" #+end_pattern
-#end_pattern=b".{39,80}" #+end_pattern
-
-#Construct regular expression to search
-re_expression=br''.join([start_pattern,br"..*",end_pattern]) #b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" #+end_pattern
-print(re_expression)
-
-#emptyhash=hashlib.md5()
-#emptyhash.update(b"")
-#empty_digest=str(emptyhash.hexdigest())
-            
-contents={}
-#contents[empty_digest]=b""
-
-
-#print(list(contents.items()))  
-#time.sleep(3)
-
-
-last_chunk=bytearray(br'')
-print("Byte Builder zeroed out to length {}".format(len(last_chunk)))
-buffer_limit=2*file_size
-
+def debugOutput(buffer_string):
+    start_list=[x.hex() for x in re.findall(b".",buffer_string[0:256:], re.DOTALL)]
+    for row in range(0,16):
+                print(start_list[(row*16):(row+1)*16])
+                
+    print("........")
+    #One off troubleshooting adjustment for hex display alignment with linux dd
+    #e.g (b".",buffer_string[-258::]
+    #The file handle reader is eating my 0x0a newlines and the grid goes out of alignment.
+    #Solve with re.DOTALL
+    stop_list=[x.hex() for x in re.findall(b".",buffer_string[-256::], re.DOTALL)]
+    for row in range(0,16):
+        print(stop_list[(row*16):(row+1)*16])
+        
+#imagefile="/media/pwaters/56e05090-43ef-4899-826f-ccf168689305/dev/sdb27"       
 #imagefile="/media/pwaters/56e05090-43ef-4899-826f-ccf168689305/PS4/sdb27copy"
 imagefile="input/MineCraft"
-with open(imagefile, "rb") as image_handler:
-    try: 
-        for image_part in bufferedSample(image_handler, file_size):
-            buffer_string=br''.join([last_chunk,image_part])
-            new_length=len(buffer_string)
-            #print("Total Byte Builder Length {}".format(new_length))
-            #Show Start and End of current sample read
-            #5243136        
+#imagefile="input/BedRockTest"
+#imagefile="/media/pwaters/56e05090-43ef-4899-826f-ccf168689305/PS4/sdb7copy"
 
-            #start_list=[x.hex() for x in re.findall(b".",buffer_string[0:256:], re.DOTALL)]
-            #for row in range(0,16):
-            #    print(start_list[(row*16):(row+1)*16])
-            #    
-            #print("........")
-        
-            #One off troubleshooting adjustment for hex display alignment with linux dd
-            #e.g (b".",buffer_string[-258::]
-            #The file handle reader is eating my 0x0a newlines and the grid goes out of alignment.
-            #Solve with re.DOTALL
-            #stop_list=[x.hex() for x in re.findall(b".",buffer_string[-256::], re.DOTALL)]
-            #for row in range(0,16):
-            #    print(stop_list[(row*16):(row+1)*16])
-             
-            #print("Sample Chunks of lengths {} and {}".format(len(start_list),len(stop_list)))
+#file_size_in_mb=115 #(5,30,60,90...)
+#bytes_per_part=32
+#sizecode=hex(file_size_in_mb*bytes_per_part)
 
-            last_chunk=buffer_string
-            if (new_length > buffer_limit):
-                trunc_start=new_length-buffer_limit 
-                print("length {} slice {}:".format(new_length,trunc_start))
-                buffer_string=bytes(buffer_string[trunc_start::])
+#be00 = 48640 * 32768 = 1593835520 = 1.5GB  1520
+#bd04 = 48388 * 32768 = 1585577984 =1.5 GB 1512
+#0e43 = 3651 * 32768 = 115MB
+#0b40 = 2880 / 90 = 32 = 43 mb
+#0562  = 1378 * 32768 = 45154304
+#0870 = 1920 / 60 = 32
+#03c0 = 960 / 30 = 32
+#00be = 190 * 32768 = 6225920, 6 MB
+#00a0 = 160 / 5 = 32
+#0008 = 8*32768 = 262144
+
+sizecode=hex(63004)
+print(sizecode)
+bytes_per_block=32*1024
+file_size=int(sizecode,16)*bytes_per_block
+suffix_length=file_size-88
+print("File {} Suffix {}".format(file_size, suffix_length))
+#file_size=100
+#time.sleep(2)
+
+
+#TODO - Why does the block device not always match on mor ethan one row of bytes
+#Comment out later lines for less specificity
+minecraft_dump=[]
+#minecraft_dump.append(b'(')
+minecraft_dump.append(b''.join([b'\x01\x00\x00\x00\x00\x00\x00\x00\x0b',b'.',b'\x33\x01\x00\x00\x00\x00'])) #\x2a is failing to be treated properly by re
+#(b'\x01\x00\x00\x00\x00\x00\x00\x00\x0b\x2a\x33\x01\x00\x00\x00\x00') 
+minecraft_dump.append(b''.join([b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',b'.',b'\x00\x0d\x00\x00\x00'])) #Bigger file ends 000001000d000000, smaller file 000000000d000000
+#####################(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0d\x00\x00\x00')
+minecraft_dump.append(b''.join([b'\x00',b'.',b'.',b'\x00',b'.',b'\x00\x00\x00',b'\x01\x00\x00\x00\x00\x00\x00\x00'])) #Older file starts 0080000001 new file starts 000010000
+#######################(b'\x00\x80\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00')
+minecraft_dump.append(b''.join([b'.',b'.',b'\x00\x00\x00\x00\x00\x00',b'(..)',b'\x00\x00\x00\x00\x00\x00'])) #This line contains the file size indicator, also may begin 0004 or 4000
+#######################(b''.join([b'\x00\x04\x00\x00\x00\x00\x00\x00',b'..',b'.',b'\x00\x00\x00\x00\x00']))
+minecraft_dump.append(b''.join([b'.',b'.',b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'])) # bigger file begins 0001, smaller file begins 0017 the rest are zeros, 
+#but all following lines differ enormously between big/small though small/small and big/big have similarities
+#######################(b'\x17\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+
+minecraft_dump.append(b'\x00\x00\x01\x00\x1e\x00\x01\x00')
+#minecraft_dump.append(b''.join([b'.{', bytes(suffix_length), b'}']))
+#minecraft_dump.append(b')')
+minecraft_dump.append(b'.*')
+
+#TODO anchor end on anything with more than ~16 - 64 byte chunks of zeros... file boundary.
+#end_pattern=b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' #+end_pattern
+
+first_run=b''.join(minecraft_dump)      
+print("Minecraft pattern {}".format(minecraft_dump))
+del(minecraft_dump[-1])
+#print(minecraft_dump)
+
+last_chunk=bytearray(br'')
+buffer_limit=2*file_size
+
+results=[];
+result_hash=b'';
+try: 
+    #for image_part in bufferedSample(image_handler, file_size):
+    for image_part in backNibbles(imagefile, buffer_limit):
+
+        try:
+            result=re.search(first_run, image_part, re.DOTALL)   
+            #print("Done {} \n {}".format(len(results),contents.keys()))
+            #time.sleep(1)
+            #Take results from this chunk and write a file
+            #for result in results:
+            if result: 
+                print("found something")
+                time.sleep(2)
+                #debugOutput(image_part)
+                #print(result)
+                #print(result.groups())
+                fsize=int(struct.unpack('<h',result.group(1))[0])
+                print("File Claims it should be {}, but I have {} bytes".format(fsize*32768, len(result.group(0))))
+              
+              
+                size_byte_array=[b'\d{',bytes(str(int(((fsize*32768)-88)/16)),'utf-8'),b'}']
+                size_byte=b''.join(size_byte_array)
+                print("Sizes {} {} \n{}".format(len(size_byte_array), len(size_byte), size_byte))
+                time.sleep(2)
+
+                minecraft_dump.append(size_byte)
+                print("Now {}".format(minecraft_dump))
+                second_run=b''.join(minecraft_dump)      
+
+                print(second_run)
+                time.sleep(2)
+                actual=re.findall(second_run,result.group(0))
+                
+                if actual:
+                    print("ACTUAL {}".format(actual[0]))
+                    time.sleep(2)
+                else:
+                    print("nope")
             
-                #print(buffer_string)
-            try:
-                results=re.findall(br"".join([re_expression]), buffer_string, re.DOTALL)   
-                print("Done {} \n {}".format(len(results),contents.keys()))
-                #time.sleep(1)
-                #Take results from this chunk and append to found dictionary
-                for result in results:
-                    hasher=hashlib.md5()
-                    hasher.update(result)
-                    result_hash=hasher.hexdigest()
-                    contents[result_hash]=bytes(result)  
-                for entry in contents.values():
-                    print("Length {} | Type {} | ID {}".format(len(entry), type(entry), id(entry)))
-                for hash_result,bytesfound in list(contents.items()):
-                    with open("output/" + str(hash_result) + "", "wb") as output_handler:
-                        output_handler.write(bytesfound)
-                contents={}
-                #contents[empty_digest]=b""           
-            except (MemoryError) as e:
-                #print("interrupted. Now will write file {}".format(e))
-                #for entry in contents.values():
-                #    print("Length {} | Type {} | ID {}".format(len(entry), type(entry), id(entry)))
-                #for hash_result,bytesfound in list(contents.items()):
-                #    with open("output/" + str(hash_result) + "", "wb") as output_handler:
-                #        output_handler.write(bytesfound)
-                contents={}
-                contents[empty_digest]=b""
-            except (KeyboardInterrupt):
-                raise
-            #finally:   
-            #    print("Yay I stayed in the loop!")
-    except KeyboardInterrupt:
-        print("Interrupted, Cleaning Up")
-        for entry in contents.values():
-            print("interrupted. Now will write file {}".format(e))
-            print("Length {} | Type {} | ID {}".format(len(entry), type(entry), id(entry)))
-            for hash_result,bytesfound in list(contents.items()):
-                with open("output/" + str(hash_result) + "", "wb") as output_handler:
-                    output_handler.write(bytesfound)
+                print("{}{}".format(dir(result), type(result.groups())))
+                result_hash=hashlib.md5(result.group(0)).hexdigest()
+
+            #hasher=hashlib.new('md5')
+            #hasher.update(result)
+            #result_hash=hasher.hexdigest(result)
+                gc.collect()
+                with open("output/" + str(result_hash) + "", "wb") as output_handler:
+                    output_handler.write(bytes(result.group(0)))
+                gc.collect()
+                
+                           
+        except (MemoryError) as e:
+            gc.collect()
+            print("trying to write file, no promises though")
+            with open("output/partialresult", "wb") as output_handler:
+                output_handler.write(results[0])
+            print(e)
+                
+        except (KeyboardInterrupt):
+            raise
+        finally:   
+            print("Garbage Collection")
+            gc.collect()
+except KeyboardInterrupt:
+        #print("Interrupted, Cleaning Up")
+        #print("Length {} | Type {} | ID {}".format(len(results), type(results), id(results)))
+    for result in results:
+        with open("output/" + str(result_hash) + "", "wb") as output_handler:
+            output_handler.write(result)
+    
                 
         
-#print(contents)
-    
+
        
 
     
