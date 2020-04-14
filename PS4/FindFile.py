3a11f4c99b75cacfae4911b43c8fdc5249839cf4
philip_waters <philip.waters@oracle.com>
2020-04-14 16:31:11 -0600

    Working

--- PS4/FindFile.py
+++ PS4/FindFile.py
@@ -1,12 +1,9 @@
 import re
-import time
 import hashlib
 import gc
-from copy import deepcopy
-
-from copy import copy
 import os
 import struct
+import io
 
 #Open Sample File
 #Open Device 
@@ -18,6 +15,7 @@ import struct
 
 
 
+
 #Yield Generator to iterate over file reading small chunks
 def bufferedSample(filehandle, size=32768):
     while True:
@@ -26,7 +24,7 @@ def bufferedSample(filehandle, size=32768):
             break
         yield nextpart
         
-def deviceNibbler(nibblefile, start=0, length=3):
+def deviceNibbler(nibblefile, start=0, length=96):
     with open(nibblefile, "rb") as nibble_handler:
         nibble_handler.seek(start)
         nibble=nibble_handler.read(length)
@@ -45,6 +43,26 @@ def backNibbles(nibblefile, windowsize=10, skipbyte=0):
         start_counter=start_counter+windowsize
         gc.collect()
         yield(nibble_buffer)
+
+#Trim Files to size
+def trimBytes(byte_string, size):
+    f=io.BytesIO(byte_string)
+    return f.read(size)
+        
+def findKeyFiles(imagefile='input/BedrockLevelInfoCache.bin'):
+    key_pattern=b'\x70\x66\x73\x53\x4b\x4b\x65\x79\x08.{79}'
+    try: 
+    #for image_part in bufferedSample(image_handler, file_size):
+        for image_part in backNibbles(imagefile, 8*32768):
+        #for image_part in backNibbles('output/a5a8d8be9e623dce510eb5b55d4a285e', 88):
+            for match in re.findall(key_pattern, image_part, re.DOTALL):
+                print("found Something")
+                result_hash=hashlib.md5(match).hexdigest()
+
+                with open(result_hash + ".bin", "wb") as key_handler:
+                    key_handler.write(match)    
+    except:
+        print("error")
         
 def debugOutput(buffer_string):
     start_list=[x.hex() for x in re.findall(b".",buffer_string[0:256:], re.DOTALL)]
@@ -85,42 +103,46 @@ sizecode=hex(63004)
 print(sizecode)
 bytes_per_block=32*1024
 file_size=int(sizecode,16)*bytes_per_block
-suffix_length=file_size-88
-print("File {} Suffix {}".format(file_size, suffix_length))
-#file_size=100
-#time.sleep(2)
 
 
-#TODO - Why does the block device not always match on mor ethan one row of bytes
+
+#TODO - Why does the block device not always match on more than one row of bytes
 #Comment out later lines for less specificity
 minecraft_dump=[]
-#minecraft_dump.append(b'(')
-minecraft_dump.append(b''.join([b'\x01\x00\x00\x00\x00\x00\x00\x00\x0b',b'.',b'\x33\x01\x00\x00\x00\x00'])) #\x2a is failing to be treated properly by re
-#(b'\x01\x00\x00\x00\x00\x00\x00\x00\x0b\x2a\x33\x01\x00\x00\x00\x00') 
-minecraft_dump.append(b''.join([b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',b'.',b'\x00\x0d\x00\x00\x00'])) #Bigger file ends 000001000d000000, smaller file 000000000d000000
+
+minecraft_dump.append(b''.join([b'\x01\x00\x00\x00\x00\x00\x00\x00\x0b',b'.',b'\x33\x01\x00\x00\x00\x00'])) 
+#\x2a is failing to be treated properly by re
+######################(b'\x01\x00\x00\x00\x00\x00\x00\x00\x0b\x2a\x33\x01\x00\x00\x00\x00') 
+
+minecraft_dump.append(b''.join([b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',b'.',b'\x00\x0d\x00\x00\x00'])) 
+#Bigger file ends 000001000d000000, smaller file 000000000d000000
 #####################(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0d\x00\x00\x00')
-minecraft_dump.append(b''.join([b'\x00',b'.',b'.',b'\x00',b'.',b'\x00\x00\x00',b'\x01\x00\x00\x00\x00\x00\x00\x00'])) #Older file starts 0080000001 new file starts 000010000
+
+minecraft_dump.append(b''.join([b'\x00',b'.',b'.',b'\x00',b'.',b'\x00\x00\x00',b'\x01\x00\x00\x00\x00\x00\x00\x00'])) 
+#Older file starts 0080000001 new file starts 000010000
 #######################(b'\x00\x80\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00')
-minecraft_dump.append(b''.join([b'.',b'.',b'\x00\x00\x00\x00\x00\x00',b'(..)',b'\x00\x00\x00\x00\x00\x00'])) #This line contains the file size indicator, also may begin 0004 or 4000
+
+minecraft_dump.append(b''.join([b'.',b'.',b'\x00\x00\x00\x00\x00\x00',b'(..)',b'\x00\x00\x00\x00\x00\x00'])) 
+#This line contains the file size indicator, also may begin 0004 or 4000
 #######################(b''.join([b'\x00\x04\x00\x00\x00\x00\x00\x00',b'..',b'.',b'\x00\x00\x00\x00\x00']))
-minecraft_dump.append(b''.join([b'.',b'.',b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'])) # bigger file begins 0001, smaller file begins 0017 the rest are zeros, 
+
+minecraft_dump.append(b''.join([b'.',b'.',b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'])) 
+# bigger file begins 0001, smaller file begins 0017 the rest are zeros, 
 #but all following lines differ enormously between big/small though small/small and big/big have similarities
 #######################(b'\x17\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
 
-minecraft_dump.append(b'\x00\x00\x01\x00\x1e\x00\x01\x00')
+##minecraft_dump.append(b'\x00\x00\x01\x00\x1e\x00\x01\x00')
+minecraft_dump.append(b''.join([b'\x00\x00\x01\x00',b'.',b'\x00', b'.', b'\x00']))
+#######################minecraft_dump.append(b'\x00\x00\x01\x00\x1e\x00\x01\x00')
 #minecraft_dump.append(b''.join([b'.{', bytes(suffix_length), b'}']))
-#minecraft_dump.append(b')')
+
 minecraft_dump.append(b'.*')
 
-#TODO anchor end on anything with more than ~16 - 64 byte chunks of zeros... file boundary.
-#end_pattern=b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' #+end_pattern
+minecraft_pattern=b''.join(minecraft_dump)      
 
-first_run=b''.join(minecraft_dump)      
-print("Minecraft pattern {}".format(minecraft_dump))
+#Remove greedy glob in case we want to use the pattern basis later
 del(minecraft_dump[-1])
-#print(minecraft_dump)
 
-last_chunk=bytearray(br'')
 buffer_limit=2*file_size
 
 results=[];
@@ -128,51 +150,22 @@ result_hash=b'';
 try: 
     #for image_part in bufferedSample(image_handler, file_size):
     for image_part in backNibbles(imagefile, buffer_limit):
-
         try:
-            result=re.search(first_run, image_part, re.DOTALL)   
-            #print("Done {} \n {}".format(len(results),contents.keys()))
-            #time.sleep(1)
+            result=re.search(minecraft_pattern, image_part, re.DOTALL)   
             #Take results from this chunk and write a file
-            #for result in results:
             if result: 
                 print("found something")
-                time.sleep(2)
-                #debugOutput(image_part)
-                #print(result)
-                #print(result.groups())
-                fsize=int(struct.unpack('<h',result.group(1))[0])
-                print("File Claims it should be {}, but I have {} bytes".format(fsize*32768, len(result.group(0))))
-              
-              
-                size_byte_array=[b'\d{',bytes(str(int(((fsize*32768)-88)/16)),'utf-8'),b'}']
-                size_byte=b''.join(size_byte_array)
-                print("Sizes {} {} \n{}".format(len(size_byte_array), len(size_byte), size_byte))
-                time.sleep(2)
-
-                minecraft_dump.append(size_byte)
-                print("Now {}".format(minecraft_dump))
-                second_run=b''.join(minecraft_dump)      
-
-                print(second_run)
-                time.sleep(2)
-                actual=re.findall(second_run,result.group(0))
                 
-                if actual:
-                    print("ACTUAL {}".format(actual[0]))
-                    time.sleep(2)
-                else:
-                    print("nope")
-            
+                #Extract File Size from the capture group
+                fsize=int(struct.unpack('<h',result.group(1))[0])
+                print("File Claims it should be {}, but I have {} bytes".format(fsize*32768, len(result.group(0))))              
+                
                 print("{}{}".format(dir(result), type(result.groups())))
                 result_hash=hashlib.md5(result.group(0)).hexdigest()
 
-            #hasher=hashlib.new('md5')
-            #hasher.update(result)
-            #result_hash=hasher.hexdigest(result)
                 gc.collect()
                 with open("output/" + str(result_hash) + "", "wb") as output_handler:
-                    output_handler.write(bytes(result.group(0)))
+                    output_handler.write(trimBytes(bytes(result.group(0)), fsize*32768))
                 gc.collect()
                 
                            
